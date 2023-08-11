4d0040c90dcc87d8ed158b88b049424caee86eda
Gavneet <gavneet.bhandal@gmail.com>
2023-08-11 16:28:17 -0400

    hashrouter

--- src/App.js
+++ src/App.js
@@ -21,24 +21,14 @@ import Story from "./pages/Story/Story";
 export default function App() {
   return (
     <div className="App">
-      {/* <BrowserRouter basename="ted737"> */}
       <HashRouter basename={process.env.PUBLIC_URL}>
         <Routes>
-          <Route path={process.env.PUBLIC_URL + "/"} element={<Landing />} />
-          <Route path={process.env.PUBLIC_URL + "/About"} element={<About />} />
-          <Route
-            path={process.env.PUBLIC_URL + "/Partners"}
-            element={<Partners />}
-          />
-          <Route
-            path={process.env.PUBLIC_URL + "/Speakers"}
-            element={<Speakers />}
-          />
-          <Route path={process.env.PUBLIC_URL + "/Faq"} element={<Faq />} />
-          <Route
-            path={process.env.PUBLIC_URL + "/OurStory"}
-            element={<Story />}
-          />
+          <Route path="/" element={<Landing />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Partners" element={<Partners />} />
+          <Route path="/Speakers" element={<Speakers />} />
+          <Route path="/Faq" element={<Faq />} />
+          <Route path="/OurStory" element={<Story />} />
         </Routes>
       </HashRouter>
       <Footer />
